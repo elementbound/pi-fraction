49b9eff8fc97a0f6fdc17e775b5e0e1c3344a09e
Gálffy Tamás <ezittgtx@gmail.com>
2019-02-04 03:57:50 +0100

    Added play/pause buttons

--- src/entry.js
+++ src/entry.js
@@ -3,16 +3,33 @@ const bind = require('./bind')
 const simplify = require('./simplify')
 const canvas = require('./canvas')
 
-const ITERATIONS_PER_TICK = 64
+const ITERATIONS_PER_TICK = 128
+
+const controls = (onplay, onpause) => {
+    document.querySelector('button.play').onclick = onplay
+    document.querySelector('button.pause').onclick = onpause
+}
 
 const loop = () => {
     const calculator = new PiCalculator()
     const bindings = bind.find(document)
     const drawingContext = canvas.init(document.querySelector('canvas'))
 
+    let isTicking = true
+
+    controls(
+        () => isTicking = true,
+        () => isTicking = false
+    )
+
     canvas.drawBackground(drawingContext)
 
     const tick = () => {
+        window.requestAnimationFrame(tick)
+
+        if(!isTicking)
+            return
+
         for(let i = 0; i < ITERATIONS_PER_TICK; ++i) {
             const point = [1 - 2 * Math.random(), 1 - 2 * Math.random()]
             const hit = calculator.sample(...point)
@@ -33,8 +50,6 @@ const loop = () => {
         }
 
         bind.update(bindings, data)
-
-        window.requestAnimationFrame(tick)
     }
 
     tick()
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/entry.js',
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
-     resolve: {
+    resolve: {
         extensions: ['.js'],
     },
     devtool: 'source-map',
